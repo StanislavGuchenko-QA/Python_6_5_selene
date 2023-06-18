52ef50406f5526ae583c8723de78af769b5d43c2
GuchenkoSI <mr.guchenko@mail.ru>
2023-06-18 18:11:52 +0300

    Выполнение домашней работы

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from selene import browser
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 900
-    browser.config.window_height = 900
+
     yield
+
     browser.quit()
--- tests/test_student_registration_form.py
+++ tests/test_student_registration_form.py
@@ -1,10 +1,24 @@
 import os
-
 from selene import browser, have, command
 
 
 def test_student_registration_form():
     # Открытие страницы с формой
     browser.open('/automation-practice-form')
-    # Заполнение имени
-    browser.element('#Firstname').type('Stanislav')
+    # Заполнение личных данных
+    browser.element('#firstName').type('Stanislav')
+    browser.element('#lastName').type('QA')
+    browser.element('#userEmail').type('example@mail.ru')
+    browser.all('#genterWrapper .custom-control').element_by(have.exact_text('Male')).click()
+    browser.element('#userNumber').type('8495556789')
+    # Выбор даты рождения
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__year-select [value="1994"]').click()
+    browser.element('.react-datepicker__month-select [value="9"]').click()
+    browser.element('.react-datepicker__day--028').click()
+    # Выбор предмета
+    browser.element('#subjectsInput').type('Mobile')
+    # Выбор хобби
+    browser.all('#hobbiesWrapper .custom-control').element_by(have.exact_text('Sports')).click()
+    # Загрузка файла
+    browser.element('#uploadPicture').click()
